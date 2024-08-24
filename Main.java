f09d7532bf91dd4fb71f77bc7374bdfd2cc71e48
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-08-24 09:54:27 -0700

    function

--- Main.java
+++ Main.java
@@ -13,32 +13,30 @@ public class Main implements Directions {
   public static void turnLeft(Robot fred) {
     fred.turnLeft();
   }
-
+  public static void diag(Robot fred, Direction dir) {
+    int length = 15;
+    while (fred.direction() != dir) {
+      turnLeft(fred);
+    }
+    for (int x = 0; x < length; x++) {
+      fred.putBeeper();
+      fred.move();
+      turnLeft(fred);
+      fred.move();
+      turnRight(fred);
+    }
+  }
   public static void main(String[] args) {
     // My robot's name is Fred
     Robot fred = new Robot(1, 18, North, 100);
     World.setVisible(true);
     World.setSize(35, 35);
     World.setDelay(5);
-    int length = 15;
-    for (int i = 0; i < 2; i++) {
-      for (int x = 0; x < length; x++) {
-        fred.putBeeper();
-        fred.move();
-        turnLeft(fred);
-        fred.move();
-        turnRight(fred);
-      }
-      for (int x = 0; x < length; x++) {
-        fred.putBeeper();
-        fred.move();
-        turnRight(fred);
-        fred.move();
-        turnLeft(fred);
-      }
-      turnLeft(fred);
-      turnLeft(fred);
-    }
+    
+    diag(fred, North);
+    diag(fred, East);
+    diag(fred, South);
+    diag(fred, West);
     }
 
 }
